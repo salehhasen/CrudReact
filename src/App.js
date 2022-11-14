5f6365676fe32383f1e0cda66a60bbd7494db572
saleh2 <s.elfituri@gia.ly>
2022-11-14 09:22:05 +0200

    initial project

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.css"
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
+import Navbar from './components/layout/Navbar';
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
+import NotFound from './components/pages/NotFound';
+import AddUser from './components/users/AddUser';
+import EditUser from './components/users/EditUser';
+import User from './components/users/User';
+function App(props) {
+  return (
+  <Router>
+      <div className="App">
+      <Navbar/>
+      <Routes>
+        <Route  path='/' element={<Home/>}/>
+        <Route  path='/about'  element={<About/>}/>   
+        <Route  path='/contact'  element={<Contact/>}/> 
+        <Route  path='/users/add' element={<AddUser/>}/>  
+        <Route  path='/users/Edit/:id' element={<EditUser/>}/> 
+        <Route  path='/users/:id' element={<User/>}/>
+        <Route path='*' element={<NotFound/>}/>
+      </Routes>
+    </div>
+  </Router>
+  );
+}
+
+export default App;
